aa0ff470c5f4a6dca696c66fd83e03a39c6be633
rachell <rachell.calhoun@gmail.com>
2016-02-17 18:49:55 +0900

    fixed header in basehtml, fixed ourstory

--- llisite/urls.py
+++ llisite/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
     # Examples:
@@ -8,4 +11,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
      url(r'', include('content.urls')),
-]
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
