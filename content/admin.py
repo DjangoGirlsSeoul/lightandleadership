731126cc50a46051130981ae453e79aa17832315
JiwonHwang <rhasu2003@gmail.com>
2016-02-19 00:38:44 +0900

    admin.py : register Ourteam

--- content/admin.py
+++ content/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Ourstory
-# Register your models here.
+from .models import Ourstory, Ourteam
+
 admin.site.register(Ourstory)
+admin.site.register(Outteam)
