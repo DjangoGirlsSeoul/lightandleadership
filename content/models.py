6f6a9a5c0e8b7bae807daa46dc0a67e32b06b5fc
rachell <rachell.calhoun@gmail.com>
2016-03-02 18:01:13 +0900

    fixed background colors and layout for ourstory

--- content/models.py
+++ content/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class OurStory(models.Model):
     img = models.ImageField(blank=True, null=True)
     text = models.TextField()
-    color = models.CharField(max_length=6, blank=True)
+    color = models.CharField(max_length=20, blank=True)
     order = models.PositiveIntegerField()
 
 
