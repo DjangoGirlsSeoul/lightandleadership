904b2b92398cd3d594676b4d771bca900ce7a89e
Jiwon Hwang <rhasu2003@gmail.com>
2016-03-09 14:18:39 +0900

    content/migrations/0015_merge.py

---
+++ content/migrations/0015_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('content', '0006_merge'),
+        ('content', '0014_auto_20160305_0931'),
+    ]
+
+    operations = [
+    ]
