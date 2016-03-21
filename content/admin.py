34390e4cdb6ab6050e12a42e21b783dd7d10d2da
rachell calhoun <rachell.calhoun@gmail.com>
2016-03-21 16:36:20 +0900

    ethicalpage setup

--- content/admin.py
+++ content/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import OurStory, OurTeam, EduProgram
+from .models import OurStory, OurTeam, EduProgram, EthicalPost
 # Register your models here.
 
 class EduProgramAdmin(admin.ModelAdmin):
@@ -10,3 +10,4 @@ class EduProgramAdmin(admin.ModelAdmin):
 admin.site.register(OurStory)
 admin.site.register(OurTeam)
 admin.site.register(EduProgram, EduProgramAdmin)
+admin.site.register(EthicalPost)
