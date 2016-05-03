bb03589f4f44e9716f47ed80ba2c0e3c88e0910b
soeun <mojosoeun@gmail.com>
2016-05-03 17:25:12 +0900

    add migration file

--- content/admin.py
+++ content/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import OurStory, OurTeam, EduProgram, EthicalPost, VolunteerPeru, VolunteerOpenPosition, VolunteerAbout, CustomPage, FooterInfo
+from .models import OurStory, OurTeam, EduProgram, EthicalPost, VolunteerPeru, VolunteerOpenPosition, VolunteerAbout, CustomPage,Home,FooterInfo
 from django.contrib.flatpages.admin import FlatpageForm, FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
 from tinymce.widgets import TinyMCE
